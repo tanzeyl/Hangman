9b076c0b4520f7e063876b394810fbb9a2f306c5
Tanzeel Khan <ktanzeel80@gmail.com>
2021-05-08 19:59:33 +0530

    Added all required files

---
+++ hangman.py
@@ -0,0 +1,36 @@
+def hangman(word):
+    wrong = 0
+    stages = ["                           ",
+              "___________________________",
+              "|                          ",
+              "|           |              ",
+              "|           0              ",
+              "|          /|\             ",
+              "|          / \             ",
+              "|                          ",]
+    remaining_letters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("Welome to Hangman.\n")
+    while wrong < len(stages) - 1:
+        print("\n\n\n")
+        choice = input("Guess a character.\n")
+        if choice in remaining_letters:
+            ch_ind = remaining_letters.index(choice)
+            board[ch_ind] = choice
+            remaining_letters[ch_ind] = "$"
+        else:
+            wrong += 1
+        print("Your progress: ")
+        print("".join(board))
+        print("\n".join(stages[0:wrong+1]))
+        if "_" not in board:
+            print("You win.\n")
+            print("".join(board))
+            win = True
+            break
+    if not win:
+        print("You lost, word was {}.".format(word))
+
+
+hangman("Tanzeel")
